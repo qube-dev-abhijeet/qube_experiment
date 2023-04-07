0581d0d7e6c908a19b9255fa57d3c538f98aafa5
Abhijeet Sharma <abhijeetsharma@Abhijeets-MacBook-Air.local>
2023-04-07 18:28:17 +0530

    Added change

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require("supertest");
 var app = require("../index.js");
-describe("GET /", function() {
- it("respond with hello world", function(done) {
+describe('GET /', function() {
+    it('respond with hello world', function(done) {
     //navigate to root and check the the response is "hello world"
-    request(app).get("/").expect("hello world11", done);
- });
-});
+    request(app).get('/').expect('hello world121', done);
+    });
+   });
