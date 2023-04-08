5b7f2bfe8f9782e26841b16574c12e04a4e393bb
qube-dev-abhijeet <abhijeet.sharma@qubehealth.in>
2023-04-08 12:01:54 +0530

    Added change

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require("supertest");
 var app = require("../index.js");
 describe("GET /", function() {
  it("respond with hello world", function(done) {
- //navigate to root and check the the respon1se1111111 is `hello world"
+ //navigate to root and check the the respon1s1e1111111 is `hello world"
  request(app).get("/").expect("hello world", done);
  });
 });
